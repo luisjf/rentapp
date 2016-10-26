6f508336bac1b2b1ba3d36dbdebcfcb51ee92cda
jose.luis <jose.luis@cleteci.com>
2016-10-26 09:47:23 -0400

    The basic update and delete of the models were included

--- rentals/api/urls.py
+++ rentals/api/urls.py
@@ -5,34 +5,54 @@ from rentals.api.views import (
 
     TenantListAPIView,
     TenantDetailAPIView,
+    TenantUpdateAPIView,
+    TenantDeleteAPIView,
 
     PropertyListAPIView,
     PropertyDetailAPIView,
+    PropertyUpdateAPIView,
+    PropertyDeleteAPIView,
 
     LocalListAPIView,
     LocalDetailAPIView,
+    LocalUpdateAPIView,
+    LocalDeleteAPIView,
 
     ContractListAPIView,
     ContractDetailAPIView,
+    ContractUpdateAPIView,
+    ContractDeleteAPIView,
 
     PaymentListAPIView,
-    PaymentDetailAPIView
+    PaymentDetailAPIView,
+    PaymentUpdateAPIView,
+    PaymentDeleteAPIView
 )
 
 urlpatterns = [
     url(r'^tenant/$', TenantListAPIView.as_view(), name='list'),
     url(r'^tenant/(?P<pk>[\w-]+)/$', TenantDetailAPIView.as_view(), name='detail'),
+    url(r'^tenant/(?P<pk>[\w-]+)/edit/$', TenantUpdateAPIView.as_view(), name='update'),
+    url(r'^tenant/(?P<pk>[\w-]+)/delete/$', TenantDeleteAPIView.as_view(), name='delete'),
 
 
     url(r'^property/$', PropertyListAPIView.as_view()),
     url(r'^property/(?P<pk>[\w-]+)/$', PropertyDetailAPIView.as_view(), name='detail'),
+    url(r'^property/(?P<pk>[\w-]+)/edit/$', PropertyUpdateAPIView.as_view(), name='update'),
+    url(r'^property/(?P<pk>[\w-]+)/delete/$', PropertyDeleteAPIView.as_view(), name='delete'),
 
     url(r'^local/$', LocalListAPIView.as_view()),
     url(r'^local/(?P<pk>[\w-]+)/$', LocalDetailAPIView.as_view(), name='detail'),
+    url(r'^local/(?P<pk>[\w-]+)/edit/$', LocalUpdateAPIView.as_view(), name='update'),
+    url(r'^local/(?P<pk>[\w-]+)/delete/$', LocalDeleteAPIView.as_view(), name='delete'),
 
     url(r'^contract/$', ContractListAPIView.as_view()),
     url(r'^contract/(?P<pk>[\w-]+)/$', ContractDetailAPIView.as_view(), name='detail'),
+    url(r'^contract/(?P<pk>[\w-]+)/edit/$', ContractUpdateAPIView.as_view(), name='update'),
+    url(r'^contract/(?P<pk>[\w-]+)/delete/$', ContractDeleteAPIView.as_view(), name='delete'),
 
     url(r'^payment/$', PaymentListAPIView.as_view()),
     url(r'^payment/(?P<pk>[\w-]+)/$', PaymentDetailAPIView.as_view(), name='detail'),
+    url(r'^payment/(?P<pk>[\w-]+)/edit/$', PaymentUpdateAPIView.as_view(), name='update'),
+    url(r'^payment/(?P<pk>[\w-]+)/delete/$', PaymentDeleteAPIView.as_view(), name='delete'),
 ]
--- rentals/api/views.py
+++ rentals/api/views.py
@@ -1,7 +1,15 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+    )
 
 from rentals.models import Tenant, Property, Local, Contract, Payment
 from .serializers import TenantSerializer, PropertySerializer, LocalSerializer, ContractSerializer, PaymentSerializer
+
+
+#TENANT Views
 
 class TenantListAPIView(ListAPIView):
     queryset = Tenant.objects.all()
@@ -12,6 +20,17 @@ class TenantDetailAPIView(RetrieveAPIView):
     serializer_class = TenantSerializer
     #lookup_field = ""
 
+class TenantUpdateAPIView(UpdateAPIView):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+
+class TenantDeleteAPIView(DestroyAPIView):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+
+
+#PROPERTY Views
+
 class PropertyListAPIView(ListAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
@@ -20,6 +39,17 @@ class PropertyDetailAPIView(RetrieveAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
     #lookup_field = ""
+
+class PropertyUpdateAPIView(UpdateAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+class PropertyDeleteAPIView(DestroyAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+
+#LOCAL Views
 
 class LocalListAPIView(ListAPIView):
     queryset = Local.objects.all()
@@ -30,6 +60,17 @@ class LocalDetailAPIView(RetrieveAPIView):
     serializer_class = LocalSerializer
     #lookup_field = ""
 
+class LocalUpdateAPIView(UpdateAPIView):
+    queryset = Local.objects.all()
+    serializer_class = LocalSerializer
+
+class LocalDeleteAPIView(DestroyAPIView):
+    queryset = Local.objects.all()
+    serializer_class = LocalSerializer
+
+
+#CONTRACT Views
+
 class ContractListAPIView(ListAPIView):
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
@@ -39,6 +80,17 @@ class ContractDetailAPIView(RetrieveAPIView):
     serializer_class = ContractSerializer
     #lookup_field = ""
 
+class ContractUpdateAPIView(UpdateAPIView):
+    queryset = Contract.objects.all()
+    serializer_class = ContractSerializer
+
+class ContractDeleteAPIView(DestroyAPIView):
+    queryset = Contract.objects.all()
+    serializer_class = ContractSerializer
+
+
+#PAYMENT Views
+
 class PaymentListAPIView(ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
@@ -47,3 +99,11 @@ class PaymentDetailAPIView(RetrieveAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     #lookup_field = ""
+
+class PaymentUpdateAPIView(UpdateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class PaymentDeleteAPIView(DestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
